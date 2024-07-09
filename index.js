a91e20b68ddee8d796f42ffdd1db01647dfd1dcd
Reza Gholami <gholamireza712@gmail.com>
2024-07-09 23:52:16 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+import { getCookie } from "./utils/cookie.js";
+import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/stringFunc.js";
+
+const loginButton = document.getElementById("login");
+const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
+const searchButton = document.querySelector("button");
+const inputBox = document.querySelector("input");
+const listItem = document.querySelectorAll("li");
+
+let allProducts = null;
+let category = "all";
+let search = "";
+
+const showProducts = (products) => {
+  mainContent.innerHTML = "";
+  products.forEach((product) => {
+    const jsx = `
+    <div>
+        <img alt=${product.title} src=${product.image}>
+        <h4>${shortenText(product.title)}</h4>
+        <div id="price">
+            <p>$ ${product.price}</p>
+            <button> 
+                Buy
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+        </div>
+        <div id="rate">
+            <i class="fa-solid fa-star"></i>
+            <span>${product.rating.rate}</span>
+        </div>
+        <div id="count">
+            <i class="fa-solid fa-user"></i>
+            <span>${product.rating.count}</span>
+        </div>
+    </div>
+    
+    `;
+    mainContent.innerHTML += jsx;
+  });
+};
+
+const init = async () => {
+  const cookie = getCookie();
+  if (cookie) {
+    loginButton.style.display = "none";
+  } else {
+    dashboardButton.style.display = "none";
+  }
+  allProducts = await getData("products");
+  showProducts(allProducts);
+};
+const filterProducts = () => {
+  const filteredProducts = allProducts.filter((product) => {
+    if (category === "all") {
+      return product.title.toLowerCase().includes(search);
+    } else {
+      return (
+        product.title.toLowerCase().includes(search) &&
+        product.category.toLowerCase() === category
+      );
+    }
+  });
+  showProducts(filteredProducts);
+};
+const searchHandler = () => {
+  search = inputBox.value.trim().toLowerCase();
+  filterProducts();
+};
+const filterHandler = (event) => {
+  category = event.target.innerText.toLowerCase();
+  listItem.forEach((li) => {
+    li.innerText.toLowerCase() === category
+      ? (li.classList = "selected")
+      : (li.classList = "");
+  });
+  filterProducts();
+};
+document.addEventListener("DOMContentLoaded", init);
+searchButton.addEventListener("click", searchHandler);
+listItem.forEach((li) => li.addEventListener("click", filterHandler));
